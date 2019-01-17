a64a947774b851de15556b1f96ba2b8e5e0006b7
akqbi168 <akqbi168@gmail.com>
2019-01-17 13:49:53 +0900

    [Finish] 5-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  # get 'users/show' ←いらない(devise_forで設定しているため)
+
   devise_for :users
   root 'post_images#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
 	resource :favorites, only: [:create, :destroy]
 	resource :post_comments, only: [:create, :destroy]
       # post_commentsのshowページは必要ない（コメントの詳細ページは作成しない）ため、idを受け渡す必要がなくresourceとしています
   end
+
+  resources :users, only: [:show]
 
 end
